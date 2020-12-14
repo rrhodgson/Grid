1c4e17a63147b97d0ba8943c82fa892a9ce2a512
Raoul Hodgson <raoul.hodgson@ed.ac.uk>
2020-12-14 15:49:54 +0000

    Added verbose prints to MADWF function

--- Grid/qcd/action/fermion/MADWF.h
+++ Grid/qcd/action/fermion/MADWF.h
@@ -91,22 +91,36 @@ class MADWF
     std::cout << GridLogMessage<< "  MADWF-like algorithm                           " << std::endl;
     std::cout << GridLogMessage<< " ************************************************" << std::endl;
 
+std::cout << "    FermionFieldi    c0i(Mati.GaugeGrid()); // 4d " << std::endl;
     FermionFieldi    c0i(Mati.GaugeGrid()); // 4d 
+std::cout << "    FermionFieldi    y0i(Mati.GaugeGrid()); // 4d" << std::endl;
     FermionFieldi    y0i(Mati.GaugeGrid()); // 4d
+std::cout << "    FermionFieldo    c0 (Mato.GaugeGrid()); // 4d " << std::endl;
     FermionFieldo    c0 (Mato.GaugeGrid()); // 4d 
+std::cout << "    FermionFieldo    y0 (Mato.GaugeGrid()); // 4d" << std::endl;
     FermionFieldo    y0 (Mato.GaugeGrid()); // 4d
 
+std::cout << "    FermionFieldo    A(Mato.FermionGrid()); // Temporary outer" << std::endl;
     FermionFieldo    A(Mato.FermionGrid()); // Temporary outer
+std::cout << "    FermionFieldo    B(Mato.FermionGrid()); // Temporary outer" << std::endl;
     FermionFieldo    B(Mato.FermionGrid()); // Temporary outer
+std::cout << "    FermionFieldo    b(Mato.FermionGrid()); // 5d source" << std::endl;
     FermionFieldo    b(Mato.FermionGrid()); // 5d source
 
+std::cout << "    FermionFieldo    c(Mato.FermionGrid()); // PVinv source; reused so store" << std::endl;
     FermionFieldo    c(Mato.FermionGrid()); // PVinv source; reused so store
+std::cout << "    FermionFieldo    defect(Mato.FermionGrid()); // 5d source" << std::endl;
     FermionFieldo    defect(Mato.FermionGrid()); // 5d source
 
+std::cout << "    FermionFieldi   ci(Mati.FermionGrid()); " << std::endl;
     FermionFieldi   ci(Mati.FermionGrid()); 
+std::cout << "    FermionFieldi   yi(Mati.FermionGrid()); " << std::endl;
     FermionFieldi   yi(Mati.FermionGrid()); 
+std::cout << "    FermionFieldi   xi(Mati.FermionGrid()); " << std::endl;
     FermionFieldi   xi(Mati.FermionGrid()); 
+std::cout << "    FermionFieldi srci(Mati.FermionGrid()); " << std::endl;
     FermionFieldi srci(Mati.FermionGrid()); 
+std::cout << "    FermionFieldi   Ai(Mati.FermionGrid()); " << std::endl;
     FermionFieldi   Ai(Mati.FermionGrid()); 
 
     RealD m=Mati.Mass();
@@ -114,6 +128,7 @@ class MADWF
     ///////////////////////////////////////
     //Import source, include Dminus factors
     ///////////////////////////////////////
+std::cout << "    Mato.ImportPhysicalFermionSource(src4,b); " << std::endl;
     Mato.ImportPhysicalFermionSource(src4,b); 
     std::cout << GridLogMessage << " src4 " <<norm2(src4)<<std::endl;
     std::cout << GridLogMessage << " b    " <<norm2(b)<<std::endl;
@@ -125,23 +140,34 @@ class MADWF
       ///////////////////////////////////////
       // Set up c0 from current defect
       ///////////////////////////////////////
+std::cout << "      PauliVillarsSolvero(Mato,defect,A);" << std::endl;
       PauliVillarsSolvero(Mato,defect,A);
+std::cout << "      Mato.Pdag(A,c);" << std::endl;
       Mato.Pdag(A,c);
+std::cout << "      ExtractSlice(c0, c, 0 , 0);" << std::endl;
       ExtractSlice(c0, c, 0 , 0);
 
       ////////////////////////////////////////////////
       // Solve the inner system with surface term c0
       ////////////////////////////////////////////////
       ci = Zero();  
+std::cout << "      convert(c0,c0i); // Possible precison change" << std::endl;
       convert(c0,c0i); // Possible precison change
+std::cout << "      InsertSlice(c0i,ci,0, 0);" << std::endl;
       InsertSlice(c0i,ci,0, 0);
 
       // Dwm P y = Dwm x = D(1) P (c0,0,0,0)^T
+std::cout << "      Mati.P(ci,Ai);" << std::endl;
       Mati.P(ci,Ai);
+std::cout << "      Mati.SetMass(1.0);      Mati.M(Ai,srci);      Mati.SetMass(m);" << std::endl;
       Mati.SetMass(1.0);      Mati.M(Ai,srci);      Mati.SetMass(m);
+std::cout << "      SchurSolveri(Mati,srci,xi,Guesseri); " << std::endl;
       SchurSolveri(Mati,srci,xi,Guesseri); 
+std::cout << "      Mati.Pdag(xi,yi);" << std::endl;
       Mati.Pdag(xi,yi);
+std::cout << "      ExtractSlice(y0i, yi, 0 , 0);" << std::endl;
       ExtractSlice(y0i, yi, 0 , 0);
+std::cout << "      convert(y0i,y0); // Possible precision change" << std::endl;
       convert(y0i,y0); // Possible precision change
 
       //////////////////////////////////////
@@ -149,24 +175,31 @@ class MADWF
       // Build Pdag PV^-1 Dm P [-sol4,c2,c3... cL]
       //////////////////////////////////////
       c0 = - y0;
+std::cout << "      InsertSlice(c0, c, 0   , 0);" << std::endl;
       InsertSlice(c0, c, 0   , 0);
 
       /////////////////////////////
       // Reconstruct the bulk solution Pdag PV^-1 Dm P 
       /////////////////////////////
+std::cout << "      Mato.P(c,B);" << std::endl;
       Mato.P(c,B);
+std::cout << "      Mato.M(B,A);" << std::endl;
       Mato.M(B,A);
+std::cout << "      PauliVillarsSolvero(Mato,A,B);" << std::endl;
       PauliVillarsSolvero(Mato,A,B);
+std::cout << "      Mato.Pdag(B,A);" << std::endl;
       Mato.Pdag(B,A);
 
       //////////////////////////////
       // Reinsert surface prop
       //////////////////////////////
+std::cout << "      InsertSlice(y0,A,0,0);" << std::endl;
       InsertSlice(y0,A,0,0);
 
       //////////////////////////////
       // Convert from y back to x 
       //////////////////////////////
+std::cout << "      Mato.P(A,B);" << std::endl;
       Mato.P(A,B);
 
       //         sol5' = sol5 + M^-1 defect
@@ -179,6 +212,7 @@ class MADWF
       std::cout << GridLogMessage << " delta    "<<norm2(B)<<std::endl;
 
        // New defect  = b - M sol5
+std::cout << "       Mato.M(sol5,A);" << std::endl;
        Mato.M(sol5,A);
        defect = b - A;
 
